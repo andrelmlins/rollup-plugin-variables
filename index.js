004b04fc4c167658b70791fa8f7d5698f7994849
André Lins <andrelucas01@hotmail.com>
2020-03-09 14:44:50 -0300

    Inserting prettier and gitgnore configs

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const fs = require("fs");
-const dotenv = require("dotenv");
-const resolverPath = require("./utils/resolverPath");
-const replaceMask = require("./utils/replaceMask");
-const createMask = require("./utils/createMask");
-const runFunctions = require("./utils/runFunctions");
-const jsonToDotNotate = require("./utils/jsonToDotNotate");
+const fs = require('fs');
+const dotenv = require('dotenv');
+const resolverPath = require('./utils/resolverPath');
+const replaceMask = require('./utils/replaceMask');
+const createMask = require('./utils/createMask');
+const runFunctions = require('./utils/runFunctions');
+const jsonToDotNotate = require('./utils/jsonToDotNotate');
 
 function compilerVariables(path, format) {
-  if (format === "js") {
+  if (format === 'js') {
     const fileConfig = require(path);
 
-    if (typeof fileConfig === "function") {
+    if (typeof fileConfig === 'function') {
       return jsonToDotNotate(fileConfig());
     }
 
     return jsonToDotNotate(fileConfig);
-  } else if (format === "env") {
+  } else if (format === 'env') {
     const file = fs.readFileSync(path);
 
     return dotenv.parse(file);
   }
 
   return jsonToDotNotate(
-    JSON.parse(fs.readFileSync(path, { encoding: "utf-8" }))
+    JSON.parse(fs.readFileSync(path, { encoding: 'utf-8' }))
   );
 }
 
 function transformVariables(options = {}) {
-  let optionsDefault = { fileName: "env.js", format: "js", marker: "[[]]" };
+  let optionsDefault = { fileName: 'env.js', format: 'js', marker: '[[]]' };
   options = runFunctions({ ...optionsDefault, ...options });
 
   if (options.marker.length % 2 !== 0) {
-    console.error(new Error("Marker is invalid"));
+    console.error(new Error('Marker is invalid'));
     process.exitCode(1);
   }
 
   return {
-    name: "variables",
+    name: 'variables',
 
     transform(code) {
       const pathFileVariables = resolverPath(`${options.fileName}`);
